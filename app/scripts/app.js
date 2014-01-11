0bf450fa82992e08515158e7fce57dff5f441954
Paco Gomez Granados <pacogomezgranados@gmail.com>
2014-01-11 22:49:44 +0100

    Quick and dirty leap motion integration

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('leaptest1App', [
+angular.module('chesireApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,18 @@
 'use strict';
 
-angular.module('leaptest1App')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('chesireApp')
+    .controller('MainCtrl', function ($scope) {
+
+        $scope.fingers = 0;
+        $scope.iterations = 0;
+        var controller = new Leap.Controller();
+
+        controller.on( 'deviceFrame' , function( frame ) {
+
+            $scope.iterations++;
+            $scope.fingers = frame.fingers.length;
+            $scope.$apply();
+        });
+
+        controller.connect();
+    });
