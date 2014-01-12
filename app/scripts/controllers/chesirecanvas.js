a8ebae2d2ae37a27b2d2e48ce81bafba85c35a27
Paco Gomez Granados <pacogomezgranados@gmail.com>
2014-01-12 17:24:20 +0100

    Creating canvas with threes

--- app/scripts/controllers/chesirecanvas.js
+++ app/scripts/controllers/chesirecanvas.js
@@ -2,10 +2,23 @@
 
 angular.module('chesireApp')
 
-.controller('ChesirecanvasCtrl', function ($scope) {
+.controller('ChesirecanvasCtrl', function ($scope, $timeout, Three) {
 
     $scope.init = function(element) {
 
-        element.append('test');
+        //Timout to make sure DOM is created for the directive
+        $timeout(function() {
+
+            element.addClass('chesirecanvas');
+            var height = element[0].offsetHeight,
+                width = element[0].offsetHeight;
+
+            $scope.scene = new Three.Scene();
+            $scope.camera = new Three.PerspectiveCamera( 75, width / height, 0.1, 1000 );
+            $scope.renderer = new Three.WebGLRenderer();
+
+            $scope.renderer.setSize( width, height );
+            element.append($scope.renderer.domElement);
+        });
     };
 });
--- app/scripts/directives/chesirecanvas.js
+++ app/scripts/directives/chesirecanvas.js
@@ -5,7 +5,6 @@ angular.module('chesireApp')
 .directive('chesirecanvas', function () {
 
     return {
-        templateUrl: 'views/chesirecanvas.html',
         restrict: 'E',
         controller: 'ChesirecanvasCtrl',
         link: function postLink(scope, element) {
