7482f0d59536ce0f5aba56a7b42b081417de77f4
Paco Gomez Granados <pacogomezgranados@gmail.com>
2014-09-11 19:13:23 +0200

    Response function graph

--- app/scripts/synth-options/response-function.controller.js
+++ app/scripts/synth-options/response-function.controller.js
@@ -4,8 +4,32 @@ angular.module('chesireApp')
 
 .controller('ResponseFunctionCtrl', function ($scope, ResponseFunctions) {
 
+    var RESPONSE_VALUES_NUMBER = 20;
+
     var init = function() {
         $scope.availableResponseFunctions = ResponseFunctions;
+        $scope.$watch('selectedResponseFunction', responseFunctionChanged);
+        $scope.selectedResponseFunction = ResponseFunctions[$scope.responseFunctionInfo.name];
+    };
+
+    var responseFunctionChanged = function() {
+        calculateResponseValues();
+    };
+
+    var calculateResponseValues = function() {
+        $scope.responseFunctionValues = [];
+        var newValue;
+        var x;
+        var increment = 1/RESPONSE_VALUES_NUMBER;
+        $scope.responseFunctionIncrement = increment;
+        for(var i=0; i<RESPONSE_VALUES_NUMBER; i++) {
+            x = i*increment;
+            newValue = $scope.selectedResponseFunction.getResponse(x, $scope.responseFunctionInfo);
+            $scope.responseFunctionValues.push({
+                x: x,
+                y: newValue
+            });
+        }
     };
 
     init();
