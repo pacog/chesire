a482dd3fc33b0b003d5d3ed79ad5323d2b36709f
Paco Gomez Granados <pacogomezgranados@gmail.com>
2015-08-29 12:14:58 +0200

    Getting rid? of Jazz for midi messaging

--- app/scripts/midi/midi-api-mediator.service.js
+++ app/scripts/midi/midi-api-mediator.service.js
@@ -2,52 +2,24 @@
 
 angular.module('chesireApp')
 
-    .factory('MidiApiMediator', function($q, $document, $compile, $timeout, $rootScope, MidiAccess) {
+    .factory('MidiApiMediator', function($q, MidiAccess) {
 
         var factoryWillBeReady = $q.defer();
 
         var init = function() {
-            initJazzMidi().then(function(jazzObject) {
-                factoryWillBeReady.resolve(new MidiAccess(jazzObject));
-            });
+            if (navigator.requestMIDIAccess) {
+                navigator.requestMIDIAccess({
+                    sysex: false // TODO: try to support it
+                }).then(function(nativeMidiAccess) {
+                    factoryWillBeReady.resolve(new MidiAccess(nativeMidiAccess));
+                }, throwNoMidiSupportError);
+            } else {
+                throwNoMidiSupportError();
+            }
         };
 
-        var initJazzMidi = function() {
-            var jazzWillBeReady = $q.defer();
-            var jazzDOMElement = createJazzDOMElement();
-            insertJazzDOMElement(jazzDOMElement).then(function() {
-                var jazzObject = getJazzObjectFromDOM();
-                if(jazzObject.isJazz) {
-                    jazzWillBeReady.resolve(jazzObject);
-                } else {
-                    jazzWillBeReady.reject('Error creating the jazz midi object');
-                }
-            });
-            return jazzWillBeReady.promise;
-        };
-
-        var createJazzDOMElement = function() {
-            var jazzObjectDomElCreator = angular.element('<div midijazzobject=""></div>');
-            var newScope = $rootScope.$new();
-            return $compile(jazzObjectDomElCreator)(newScope);
-        };
-
-        var insertJazzDOMElement = function(element) {
-            //TODO: use $rootElement instead for more cohesion
-            var domElementWillBeInserted = $q.defer();
-            var body = $document.find('body').eq(0);
-            body.append(element);
-
-            $timeout(function() {
-                domElementWillBeInserted.resolve(element);
-            }, 10);
-
-            return domElementWillBeInserted.promise;
-        };
-
-        var getJazzObjectFromDOM = function() {
-
-            return angular.element(document.querySelector('#Jazz2'))[0];
+        var throwNoMidiSupportError = function() {
+            throw ('No MIDI support in your browser.');
         };
 
         init();
@@ -55,30 +27,21 @@ angular.module('chesireApp')
         return factoryWillBeReady.promise;
     })
 
-    .directive('midijazzobject', function () {
-        return {
-            template: '<object id="Jazz1" classid="CLSID:1ACE1618-1C7D-4561-AEE1-34842AA85E90" class="hidden"><object id="Jazz2" type="audio/x-jazz" class="hidden"><p style="visibility:visible;">This page requires <a href="http://jazz-soft.net>Jazz-Plugin</a> ...</p></object></object>',
-            restrict: 'A',
-            scope: {}
-        };
-    })
-
     .factory('MidiAccess', function(MidiOutput) {
 
-        var MidiAccessClass = function(JazzObject) {
-            if(!JazzObject) {
+        var MidiAccessClass = function(nativeMidiAccess) {
+            if(!nativeMidiAccess) {
                 throw 'Error creating MidiAccess object: we need the JazzMidi object';
             }
-            this.JazzObject = JazzObject;
+            this.nativeMidiAccess = nativeMidiAccess;
         };
 
         MidiAccessClass.prototype.outputs = function() {
 
-            var jazzObjectMidiOutList = this.JazzObject.MidiOutList();
             var outputs = [];
 
-            for (var i=0; i<jazzObjectMidiOutList.length; i++) {
-                outputs.push(new MidiOutput(this, jazzObjectMidiOutList[i], i));
+            for (var output of this.nativeMidiAccess.outputs.values()) {
+                outputs.push(new MidiOutput(this, output));
             }
 
             return outputs;
@@ -89,18 +52,17 @@ angular.module('chesireApp')
 
     .service('MidiOutput', function(MidiMessagesHelper) {
 
-        var jazzObject = null;
+        var outputDevice = null;
 
-        var MidiOutput = function ( midiAccess, name, index ) {
-            jazzObject = midiAccess.JazzObject;
-            this.id = '' + index + '.' + name;
-            this.name = name;
-            jazzObject.outputInUse = true;
-            jazzObject.MidiOutOpen(this.name);
+        var MidiOutput = function ( nativeMidiAccess, outputDeviceReceived) {
+            this.id = outputDeviceReceived.id;
+            this.name = outputDeviceReceived.name;
+            outputDevice = outputDeviceReceived;
         };
 
         var send = function( data ) {
-            jazzObject.MidiOutLong( data );
+            outputDevice.send(data);
+            // jazzObject.MidiOutLong( data );
         };
 
         MidiOutput.prototype.send = send;
