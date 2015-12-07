5ebf22eb18e9502171528d346ffe401b5db4ec42
Paco Gomez Granados <pacogomezgranados@gmail.com>
2015-12-07 17:16:08 +0100

    Added chords moods

--- app/scripts/scales/chord-moods.service.js
+++ app/scripts/scales/chord-moods.service.js
@@ -8,11 +8,48 @@
                 name: 'Major',
                 intervals: [true, false, false, false, true, false, false, true, false, false, false, false]
             }
-        },{
+        },
+        {
             name: 'Minor',
             value: {
                 name: 'Minor',
                 intervals: [true, false, false, true, false, false, false, true, false, false, false, false]
             }
-        }]);
+        },
+        {
+            name: '7',
+            value: {
+                name: '7',
+                intervals: [true, false, false, false, true, false, false, true, false, false, true, false]
+            }
+        },
+        {
+            name: 'm7',
+            value: {
+                name: 'm7',
+                intervals: [true, false, false, true, false, false, false, true, false, false, true, false]
+            }
+        },
+        {
+            name: 'maj7',
+            value: {
+                name: 'maj7',
+                intervals: [true, false, false, false, true, false, false, true, false, false, false, true]
+            }
+        },
+        {
+            name: '6',
+            value: {
+                name: '6',
+                intervals: [true, false, false, false, true, false, false, true, false, true, false, false]
+            }
+        },
+        {
+            name: 'm6',
+            value: {
+                name: '6',
+                intervals: [true, false, false, true, false, false, false, true, false, true, false, false]
+            }
+        }
+        ]);
 })();
