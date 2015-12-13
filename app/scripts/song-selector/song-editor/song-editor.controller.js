684f2ee1e7f3b15ae6770b252fcb10fd07b83f2d
Paco Gomez Granados <pacogomezgranados@gmail.com>
2015-12-13 18:38:14 +0100

    Chord picker integration

--- app/scripts/song-selector/song-editor/song-editor.controller.js
+++ app/scripts/song-selector/song-editor/song-editor.controller.js
@@ -4,13 +4,14 @@
     angular.module('chesireApp')
         .controller('SongEditorController', SongEditorController);
 
-    function SongEditorController(chordNames) {
+    function SongEditorController(chordNames, chordSelector) {
         var vm = this;
 
         var CHORDS_TWO_ROWS_LIMIT = 5;
 
         vm.getChordName = getChordName;
         vm.isChordInSecondRow = isChordInSecondRow;
+        vm.pickChord = pickChord;
 
         function getChordName(chord) {
             return chordNames.getChordName(chord);
@@ -18,6 +19,12 @@
 
         function isChordInSecondRow($index) {
             return ((vm.song.chords.length > CHORDS_TWO_ROWS_LIMIT) && ($index%2)===1);
+        }
+
+        function pickChord(index, chords) {
+            chordSelector.showSelector().then(function(chordChosen) {
+                chords[index] = chordChosen;
+            });
         }
 
     }
